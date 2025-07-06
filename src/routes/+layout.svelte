3085f4815671d8d6c6d2af07f112c50677f2e7a5
Sebastian Tremmel <littleperlmonk@gmail.com>
2025-07-06 19:19:03 +0200

    mobile layout korrigiert (#14)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,57 +15,55 @@
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-<div class="grid min-h-screen grid-rows-[auto_1fr_auto]">
-	{#if innerWidth >= 1280}
-		<AppBar toolbarClasses="justify-center" padding="p-2">
-			<Navigation.Bar>
-				{#each menuData as link (link.href)}
+{#if innerWidth >= 1280}
+	<AppBar toolbarClasses="justify-center" padding="p-2">
+		<Navigation.Bar>
+			{#each menuData as link (link.href)}
+				<Navigation.Tile
+					href={link.href}
+					label={link.label}
+					selected={page.url.pathname === link.href}
+					labelClasses="whitespace-nowrap !text-base"
+				>
+					<link.icon />
+				</Navigation.Tile>
+			{/each}
+		</Navigation.Bar>
+	</AppBar>
+{/if}
+
+<main class="relative h-full">
+	<div class="absolute inset-0 overflow-y-auto">
+		<div class="container mx-auto p-2 pb-16 sm:p-4">
+			<div
+				class="mx-auto max-w-3xl rounded-lg bg-white/50 p-4 shadow-lg backdrop-blur-lg dark:bg-black/50"
+				transition:slide={{ delay: 250, duration: 300, easing: quintOut, axis: 'x' }}
+			>
+				{@render children?.()}
+			</div>
+		</div>
+	</div>
+</main>
+
+<footer
+	class="border-light-500 dark:border-dark-500 bg-surface-100-900 sticky bottom-0 z-10 mt-4 dark:bg-black/50"
+>
+	{#if innerWidth > 1280}
+		<Footer />
+	{:else}
+		<Navigation.Bar>
+			{#each menuData as link (link.href)}
+				{#if link.includeOnMobile}
 					<Navigation.Tile
 						href={link.href}
 						label={link.label}
 						selected={page.url.pathname === link.href}
-						labelClasses="whitespace-nowrap !text-base"
+						labelClasses="whitespace-nowrap"
 					>
 						<link.icon />
 					</Navigation.Tile>
-				{/each}
-			</Navigation.Bar>
-		</AppBar>
+				{/if}
+			{/each}
+		</Navigation.Bar>
 	{/if}
-
-	<main class="relative h-full">
-		<div class="absolute inset-0 overflow-y-auto">
-			<div class="container mx-auto p-4 pb-16">
-				<div
-					class="mx-auto max-w-3xl rounded-lg bg-white/50 p-4 shadow-lg backdrop-blur-lg dark:bg-black/50"
-					transition:slide={{ delay: 250, duration: 300, easing: quintOut, axis: 'x' }}
-				>
-					{@render children?.()}
-				</div>
-			</div>
-		</div>
-	</main>
-
-	<footer
-		class="border-light-500 dark:border-dark-500 bg-surface-100-900 sticky bottom-0 z-10 mt-4 dark:bg-black/50"
-	>
-		{#if innerWidth > 1280}
-			<Footer />
-		{:else}
-			<Navigation.Bar>
-				{#each menuData as link (link.href)}
-					{#if link.includeOnMobile}
-						<Navigation.Tile
-							href={link.href}
-							label={link.label}
-							selected={page.url.pathname === link.href}
-							labelClasses="whitespace-nowrap"
-						>
-							<link.icon />
-						</Navigation.Tile>
-					{/if}
-				{/each}
-			</Navigation.Bar>
-		{/if}
-	</footer>
-</div>
+</footer>
